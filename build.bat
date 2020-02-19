d50e4a5f2e311e7907763634fef01d598b9d35f8
Matt Nadareski <mnadareski@outlook.com>
2020-02-19 14:29:50 -0800

    Try using built-in things

--- build.bat
+++
@@ -1,4 +0,0 @@
-@setlocal 
-@set /P TEMPVER= Enter the version number to build for:
-
-msbuild.exe .\.build\libmspack4n.msbuild /p:TheVersion=%TEMPVER%
--- test.bat
+++
@@ -1,3 +0,0 @@
-@setlocal
-
-packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe .\.deploy\libmspack4ntest.dll -verbose
